fba84b6ed228fd427ee62dfac92892012db84997
João Queiroz <joao.queiroz@hydradev.io>
2024-07-06 12:43:36 +0100

    Add custom CORS with forntend url

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,11 @@ import cors from "cors";
 
 const app = express();
 app.use(json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 app.use(routes);
 
 export { app };
