328e07f50aaebe123643f2145dde1f5253f4afd0
João Queiroz <joao.queiroz@hydradev.io>
2024-07-07 00:03:09 +0100

    Comment Seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+/* import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -87,3 +87,4 @@ main()
   .finally(async () => {
     await prisma.$disconnect();
   });
+ */
