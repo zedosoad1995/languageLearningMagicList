7b69bc660f0c08c036e2ecd794654b1654a28072
João Queiroz <joao.queiroz@hydradev.io>
2024-07-08 01:50:05 +0100

    Add transform in zod

--- src/schemas/word/editWord.ts
+++ src/schemas/word/editWord.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const editWordSchema = z
   .object({
-    original: z.string(),
-    translation: z.string(),
+    original: z.string().transform((val) => val.trim()),
+    translation: z.string().transform((val) => val.trim()),
     knowledge: z.number().min(1).max(5),
     relevance: z.number().min(1).max(5),
     isSeen: z.boolean(),
